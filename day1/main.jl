17b350733a3008bb5be4d4e30aff92d5ca9e5d83
Ethan <mail@ethanwilliams.xyz>
2022-12-15 11:36:54 +0000

    format day 1

--- day1/main.jl
+++ day1/main.jl
@@ -1,3 +1,3 @@
 open("input.txt","r") do f
-eachmatch(r"((\d+\n)+)",read(f,String)) .|> (sum∘(x->parse.(Int32,x))∘split∘(x->x.match)) |> (x->sort(x,rev=true)) |> (x->(x[1],sum(x[1:3]))) |> display
+    eachmatch(r"((\d+\n)+)",read(f,String)) .|> (sum∘(x->parse.(Int32,x))∘split∘(x->x.match)) |> (x->sort(x,rev=true)) |> (x->(x[1],sum(x[1:3]))) |> display
 end
