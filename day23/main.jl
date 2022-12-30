fae096490c92b453271d57e59f4f21205c42f326
Ethan <sk18181@bristol.ac.uk>
2022-12-30 23:26:46 +0000

    clean functions

--- day23/main.jl
+++ day23/main.jl
@@ -34,55 +34,42 @@ function fillgrid(out::String)
     lines=split(out,"\n")
     inputdims=(length(lines),length(lines[1]))
     gridsize=inputdims.*2
-    grid=fill(EMPTY,gridsize) #compare datatypes
+    grid=fill(false ,gridsize) #compare datatypes
     innergridstart=Int.(ceil.(inputdims./2))
     innergridend=innergridstart.+inputdims
     innergrid=@view grid[innergridstart[1]:innergridend[1],innergridstart[2]:innergridend[2]]
     for line=1:inputdims[1],char=1:inputdims[2]
-        innergrid[line,char]=lines[line][char]=='#' ? N : EMPTY
+        innergrid[line,char]=lines[line][char]=='#' ? true : false
     end
     return grid
 end
 
 
-function solve(out)
-    checkfirst=N
-    grid=fillgrid(out)
+function proposedmoves!(grid,movegrid,checkfirst)
     sizey,sizex=size(grid)
-    println("start grid")
-    
-
-
-    for i=1:3
-    display(grid)
     moveorder=checkorder(checkfirst)
-    movegrid=fill(STAY,sizey,sizex)
-    newgrid=fill(EMPTY,sizey,sizex)
-
     for y=2:sizey-1,x=2:sizex-1
-        
         current=@view grid[y-1:y+1,x-1:x+1]
         n=@view current[1,1:3]
         s=@view current[3,1:3]
         w=@view current[1:3,1]
         e=@view current[1:3,3]
         views=[n,s,w,e]
-
-        
         #find possible move, populate proposed move grid
-        if any(any.(map.(x->Int(x)!=0,(n,s,e,w))))
-            
+        if any(any.((n,s,e,w))) && (x->Int(x)!=0)(grid[y,x]) #if nighbors are not empty and neither is the current cell
             for dir in moveorder
-                if !(any(map(x->Int(x)!=0,views[dir]))) && (x->Int(x)!=0)(grid[y,x])
+                if !(any(map(x->Int(x)!=0,views[dir])))  #if there's any neighbors in the current search direction
                     movegrid[y,x]=Direction(dir)
                     break
                     
                 end
             end
         end
-
     end  
+end
 
+function removeConflicts!(movegrid)
+    sizey,sizex=size(movegrid)
 
     #check move grid for conflicting moves
     movetranslations=Dict(N=>(-1,0),S=>(1,0),W=>(0,-1),E=>(0,1))
@@ -113,34 +100,57 @@ function solve(out)
        
         
     end
-    println("moves")
-    #display(invalid)
-    display(movegrid)
-    
+
+end
 
 
+function makeMoves!(grid,newgrid,movegrid)
     #make moves
+    movetranslations=Dict(N=>(-1,0),S=>(1,0),W=>(0,-1),E=>(0,1))
+    sizey,sizex=size(grid)
     for y=2:sizey-1,x=2:sizex-1
         if movegrid[y,x]!=STAY
             newpos=(y,x).+ movetranslations[movegrid[y,x]]
             ny,nx=newpos
-            newgrid[ny,nx]=N
-            grid[y,x]=EMPTY
-        elseif grid[y,x]!=EMPTY
+            newgrid[ny,nx]=true
+            grid[y,x]=false
+            movegrid[y,x]=STAY
+        elseif grid[y,x]
             newgrid[y,x]=grid[y,x]
+            grid[y,x]=false
         end
-    end
-
-    temp=grid
-    grid=newgrid
-    newgrid=temp
-    #println("finalgrid")
-    #display(grid)
-    checkfirst=Direction(checkorder(checkfirst)[2])
     end
 
 end
 
+function step!(grid,newgrid,movegrid,checkfirst)
+    proposedmoves!(grid,movegrid,checkfirst)
+    removeConflicts!(movegrid)
+    makeMoves!(grid,newgrid,movegrid)
+end
+
+function solve(out)
+    checkfirst=N
+    grid=fillgrid(out)
+    sizey,sizex=size(grid)
+    println("start grid")
+    movegrid=fill(STAY,sizey,sizex)
+    newgrid=fill(false,sizey,sizex)
+    
+
+
+    for i=1:10
+        display(grid)
+        step!(grid,newgrid,movegrid,checkfirst)
+        grid,newgrid=newgrid,grid
+        checkfirst=Direction(checkorder(checkfirst)[2])
+        
+    end
+
+end
+
+
+##unit tests
 
 f=open("example2.txt")
 
