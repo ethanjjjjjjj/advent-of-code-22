538c587475d66d527ecf6be1f6c35aa5f1c3b429
Ethan <sk18181@bristol.ac.uk>
2022-12-31 01:45:19 +0000

    smaller functions, remove moves grid

--- day23/main.jl
+++ day23/main.jl
@@ -8,6 +8,8 @@
     STAY=5
 end
 
+movetranslations=Dict(N=>(-1,0),S=>(1,0),W=>(0,-1),E=>(0,1),STAY=>(0,0))
+
 function checkorder(d::Direction) #defines order to check directions in based on what you're checking first, returns no range if direction is empty
     if d==EMPTY return [] end
     D=Int(d)
@@ -23,11 +25,6 @@ function symdir(d::Direction)
     elseif d==EMPTY return "."
     end
 end
-
-
-Base.display(g::Array{Direction,2})=display(map(symdir,g))
-
-
 
 function fillgrid(out::String)
 
@@ -45,9 +42,11 @@ function fillgrid(out::String)
 end
 
 
-function proposedmoves!(grid,movegrid,checkfirst)
+function proposedmoves(grid,checkfirst)
     sizey,sizex=size(grid)
     moveorder=checkorder(checkfirst)
+    
+    moves=Dict()
     for y=2:sizey-1,x=2:sizex-1
         current=@view grid[y-1:y+1,x-1:x+1]
         n=@view current[1,1:3]
@@ -59,102 +58,89 @@ function proposedmoves!(grid,movegrid,checkfirst)
         if any(any.((n,s,e,w))) && (x->Int(x)!=0)(grid[y,x]) #if nighbors are not empty and neither is the current cell
             for dir in moveorder
                 if !(any(map(x->Int(x)!=0,views[dir])))  #if there's any neighbors in the current search direction
-                    movegrid[y,x]=Direction(dir)
+                    moves[(y,x)]=(y,x).+movetranslations[Direction(dir)]
                     break
-                    
+                end
+                if !((y,x) in keys(moves))
+                    moves[(y,x)]=(y,x)
                 end
             end
         end
-    end  
+    end
+    return moves  
 end
 
-function removeConflicts!(movegrid)
-    sizey,sizex=size(movegrid)
-
+function removeConflicts(moves)
     #check move grid for conflicting moves
-    movetranslations=Dict(N=>(-1,0),S=>(1,0),W=>(0,-1),E=>(0,1))
-    #moves list
     moveto=Dict()
-    invalid=Set()
-    #println("destinations")
-    for y=2:sizey-1,x=2:sizex-1
-        current=movegrid[y,x]
-        if current!=STAY
-            destination=(y,x).+movetranslations[current]
-           
-            #println((y,x),destination)
-            if !(destination in keys(moveto))
-                moveto[destination]=(y,x)
-            else
-                push!(invalid,(y,x))
-                push!(invalid,moveto[destination])
-            end
+    invalid=[]
+    #push destination=>from to moveto dict, if that key is already found, then add the pairing to the invalid array
+    for item in moves
+        if item[2] in keys(moveto)
+            push!(invalid,item)
+        else
+            moveto[item[2]]=item[1]
         end
     end
-    
-    #remove invalid moves from planned moves, tell elves to search in next direction next time
+    #iterate over all invalid, set their destination to be same as source (no move), as well as correct already proposed move in moveto
     for move in invalid
-        y=move[1]
-        x=move[2]
-        movegrid[y,x]=STAY
-       
+        source,dest=move
+        #store source then delete duplicate dest
+        update=moveto[dest]
+        delete!(moveto,dest)
+        #change dest so that the source does not move
+        moveto[update]=update
+        #also store the key that is invalid into movelist as an item which does not move
+        moveto[source]=source
+
+    end
+    return moveto
+end
+
+function generatemoves(grid,checkfirst)
+    return removeConflicts∘proposedmoves(grid,checkfirst)
+end
+
+function makeMoves!(grid,newgrid,moves)
+    for item in moves
+        to,from=item
+        newgrid[to[1],to[2]]=true
+        grid[from[1],from[2]]=false
+    end
+end
+
+function step!(grid,newgrid,checkfirst)
+    movesfromto=proposedmoves(grid,checkfirst)
+    movestofrom=removeConflicts(movesfromto)
+    makeMoves!(grid,newgrid,movestofrom)
+end
+
+function smallestRectangle(grid)
+    sizey,sizex=size(grid)
+    sx,sy,bx,by.=0
+    for y=1:sizey,x=1:sizex
+
         
     end
-
 end
 
-
-function makeMoves!(grid,newgrid,movegrid)
-    #make moves
-    movetranslations=Dict(N=>(-1,0),S=>(1,0),W=>(0,-1),E=>(0,1))
-    sizey,sizex=size(grid)
-    for y=2:sizey-1,x=2:sizex-1
-        if movegrid[y,x]!=STAY
-            newpos=(y,x).+ movetranslations[movegrid[y,x]]
-            ny,nx=newpos
-            newgrid[ny,nx]=true
-            grid[y,x]=false
-            movegrid[y,x]=STAY
-        elseif grid[y,x]
-            newgrid[y,x]=grid[y,x]
-            grid[y,x]=false
-        end
-    end
-
-end
-
-function step!(grid,newgrid,movegrid,checkfirst)
-    proposedmoves!(grid,movegrid,checkfirst)
-    removeConflicts!(movegrid)
-    makeMoves!(grid,newgrid,movegrid)
-end
 
 function solve(out)
     checkfirst=N
     grid=fillgrid(out)
     sizey,sizex=size(grid)
     println("start grid")
-    movegrid=fill(STAY,sizey,sizex)
     newgrid=fill(false,sizey,sizex)
     
 
 
     for i=1:10
-        display(grid)
-        step!(grid,newgrid,movegrid,checkfirst)
+        @time step!(grid,newgrid,checkfirst)
         grid,newgrid=newgrid,grid
         checkfirst=Direction(checkorder(checkfirst)[2])
-        
     end
 
 end
 
 
-##unit tests
 
-f=open("example2.txt")
-
-
-out=read(f,String)
-
-@assert solve(out) == 110
